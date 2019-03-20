e1712fe68aa1f9dd84cc8f1fe139c05faac1d62b
arvnq <arvin.quiliza@gmail.com>
2019-03-21 02:43:41 +1100

    Made custom button to insert additional properties to calc buttons

---
+++ calculator/View/CalcButton.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButton.swift
+//  calculator
+//
+//  Created by Arvin Quiliza on 3/21/19.
+//  Copyright © 2019 arvnq. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+
+}
