04449673c222dac864b71c4117c84d368a8a21f4
arvnq <arvin.quiliza@gmail.com>
2019-03-21 03:23:32 +1100

    Added animation on calcButtons for when touch has began and has ended

--- calculator/View/CalcButton.swift
+++ calculator/View/CalcButton.swift
@@ -19,8 +19,24 @@ class CalcButton: UIButton {
         layer.cornerRadius = self.bounds.height / 5
     }
     
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        <#code#>
-//    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //shrink the button
+        transform = CGAffineTransform.init(scaleX: 0.9, y: 0.9)
+        
+        super.touchesBegan(touches, with: event)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //when touch ended, animate back to default
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 0, options: .allowUserInteraction,
+                       animations: {
+                        self.transform = CGAffineTransform.identity
+                    }, completion: nil)
+        
+        super.touchesEnded(touches, with: event)
+    }
 
 }
