168d80eb963f48e3543bcd5e98cc0f7578884c87
arvnq <arvin.quiliza@gmail.com>
2019-03-22 07:15:47 +1100

    Add an input type to track last input from the calc keyboard

---
+++ calculator/Model/InputType.swift
@@ -0,0 +1,15 @@
+//
+//  KeyType.swift
+//  calculator
+//
+//  Created by Arvin Quiliza on 3/22/19.
+//  Copyright © 2019 arvnq. All rights reserved.
+//
+
+import Foundation
+
+/// Enum representing the type of input when a key is pressed
+enum InputType: String {
+    case operation
+    case operand
+}
